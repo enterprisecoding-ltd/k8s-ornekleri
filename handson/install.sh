ddc3f875889fed124f994814713a79a92983166c
Fatih Boy <fatih@enterprisecoding.com>
2020-10-07 13:25:03 +0300

    Kubernetes hands on sınavı eklendi

---
+++ handson/install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+clear
+cat << "EOF"
+==================================================================================
+   ______       _                       _                        _ _
+  |  ____|     | |                     (_)                      | (_)
+  | |__   _ __ | |_ ___ _ __ _ __  _ __ _ ___  ___  ___ ___   __| |_ _ __   __ _
+  |  __| | '_ \| __/ _ \ '__| '_ \| '__| / __|/ _ \/ __/ _ \ / _` | | '_ \ / _` |
+  | |____| | | | ||  __/ |  | |_) | |  | \__ \  __/ (_| (_) | (_| | | | | | (_| |
+  |______|_| |_|\__\___|_|  | .__/|_|  |_|___/\___|\___\___/ \__,_|_|_| |_|\__, |
+                            | |                                             __/ |
+                            |_|                                            |___/
+===================================================================================
+
+Kubernetes handson ortamı kullanıma hazırlanıyor lütfen bekleyiniz
+kubectl create -f https://raw.githubusercontent.com/enterprisecoding-ltd/k8s-ornekleri/master/handson/handson-install.yaml
+EOF
+while [[ $(kubectl get pods -n handson -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True" ]]; do printf "." && sleep 1; done
+echo ""
+echo "Kubernetes handson ortamı kullanıma hazır. Başarılar..."
