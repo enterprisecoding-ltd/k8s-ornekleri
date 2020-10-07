75975a36429b0820c4cfa3ca7295b797a31e12f0
Fatih Boy <fatih@enterprisecoding.com>
2020-10-07 19:52:52 +0300

    handons betiğine nodeport bilgisi eklendi

--- handson/install.sh
+++ handson/install.sh
@@ -18,5 +18,10 @@ EOF
 kubectl create -f https://raw.githubusercontent.com/enterprisecoding-ltd/k8s-ornekleri/master/handson/handson-install.yaml
 
 while [[ $(kubectl get pods -n handson -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True" ]]; do printf "." && sleep 1; done
+
+APP_NODE_PORT=$(kubectl get services kubernetes-handson -n handson  -o=jsonpath='{.spec.ports[0].nodePort}')
+
 echo ""
-echo "Kubernetes handson sınav ortamı kullanıma hazır. Başarılar..."
+echo "Kubernetes handson sınav ortamı kullanıma hazır."
+echo "Uygulama erişim portu : $APP_NODE_PORT"
+echo "başarılar..."
